88c6040c97afa3133d40f02aacae428860ddc71b
emilastanov <emila1998@me.com>
2017-10-24 14:41:07 +0300

    lab2

---
+++ lab2/Figure.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    /// <summary>
+    /// Фигура
+    /// </summary>
+    abstract class Figure
+    {
+        /// <summary>
+        /// Тип фигуры
+        /// </summary>
+        public string Type { get; protected set; }
+        string _Type;
+        /// <summary>
+        /// Вычисление плозщади
+        /// </summary>
+        /// <returns></returns>
+        public abstract double Area();
+        /// <summary>
+        /// Приведение к строке, переопределение метода Object.
+        /// </summary>
+        public override string ToString()
+        {
+            return this.Type + " площадью" + this.Area().ToString();
+        }
+    }
+    interface IPrint { void Print(); }
+    /// <summary>
+    /// Прямоугольник
+    /// </summary>
+    class Rectangle : Figure, IPrint
+    {
+        /// <summary>
+        /// Высота
+        /// </summary>
+        private double height;
+        /// <summary>
+        /// Ширина
+        /// </summary>
+        private double width;
+
+        /// <summary>
+        /// Основной конструктор
+        /// </summary>
+        /// <param name="height">Высота</param>
+        /// <param name="width">Ширина</param>
+        public Rectangle(double height, double width)
+        {
+            Type = "Прямоугольник";
+            this.height = height;
+            this.width = width;
+        }
+        /// <summary>
+        /// Вычисление площади.
+        /// </summary>
+        public override double Area()
+        {
+            double area = height * width;
+            return area;
+        }
+        /// <summary>
+        /// Вывод информации о фигуре в консоль.
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+    /// <summary>
+    /// Класс Квадрат.
+    /// </summary>
+    class Square : Rectangle
+    {
+        /// <summary>
+        /// Основной конструктор.
+        /// </summary>
+        /// <param name="size">Длина стороны квадрата</param>
+        public Square(double size) : base(size, size)
+        {
+            Type = "Квадрат";
+        }
+    }
+
+    /// <summary>
+    /// Класс Круг.
+    /// </summary>
+    class Circle : Figure, IPrint
+    {
+        /// <summary>
+        /// Радиус круга.
+        /// </summary>
+        private double radius;
+        /// <summary>
+        /// Основной конструктор.
+        /// </summary>
+        /// <param name="radius">Радиус круга</param>
+        public Circle(double radius)
+        {
+            Type = "Круг";
+            this.radius = radius;
+        }
+        /// <summary>
+        /// Вычисление площади.
+        /// </summary>
+        public override double Area()
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+        }
+        /// <summary>
+        /// Вывод информации о фигуре в консоль.
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine(ToString());
+        }
+    }
+}
---
+++ lab2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Figure;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rctngl = new Rectangle(10,15);
+            Square sqr = new Square(Math.Sqrt(Math.E));
+            Circle crcl = new Circle(5 / Math.Sqrt(Math.PI));
+
+            rctngl.Print();
+            sqr.Print();
+            crcl.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
